2b2a3b8bfd71e0aaccc5b295fe1c29aacbd68e67
harsh9711 <87559341+harsh9711@users.noreply.github.com>
2024-03-16 12:58:36 +0530

    hero1 changes

--- app/components/hero1.tsx
+++ app/components/hero1.tsx
@@ -7,15 +7,9 @@ const Hero1 = () => {
   return (
     <div className='pt-4 lg:pt-10 mt-40'>
       <div className='px-[20px] lg:px-[280px] mb-10'>
-        <h1 className='text-center pt-6 text-[#36485C] lg:text-[70px] lg:leading-7 mb-10'>For Seamless Onboarding &</h1>
-        <p className='text-center text-[32px] font-Jost leading-[40px] font-extrabold text-green-800 lg:text-[64px] lg:leading-[72px] mb-10'>Property Management Experience</p>
-        <h1 className='text-center font-Jost text-gray-400 text-[30px] font-normal leading-160 tracking-tighter mx-24'>Our commitment to innovation and customer satisfaction ensures that we are constantly improving and tailoring our software to meet the unique needs of the property management industry.</h1>
-
-        <div className='gap-x-6 flex w-full justify-center pt-8'>
-          <button className='bg-[#4328EB] w-1/2 lg:w-fit text-white py-4 px-8 rounded-[4px]'>
-            Try for free
-          </button>
-        </div>
+        <h1 className='text-center pt-6 text-[#36485C] lg:text-[30px] lg:leading-7 mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quae ut totam dolores adipisci ipsum corrupti fugit tenetur maxime provident suscipit nobis voluptates eligendi doloremque aliquid aliquam unde a hic.</h1>
+        <p className='text-center text-[32px] font-Jost leading-[40px] font-extrabold lg:text-[64px] lg:leading-[72px] mb-10'></p>
+        <h1 className='text-center font-Jost text-gray-400 text-[30px] font-normal leading-160 tracking-tighter mx-24'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint eos dolorum illum nemo, asperiores aliquid distinctio nesciunt hic! Voluptatum asperiores ipsa delectus voluptate temporibus sint ad labore aliquid ipsam voluptatem.</h1>
       </div>
 
    
